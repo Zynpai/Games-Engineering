8e6300c387fdd81dc73c70e0ad0c0f87eaf7d12b
Zynpai <40319172@live.napier.ac.uk>
2020-01-15 11:16:08 +0000

    SFML hello world working better version

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -2,9 +2,22 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200,200), "SFML works!");
+	float position = 0.0f;
+	sf::RenderWindow window(sf::VideoMode(400,400), "SFML works!");
 	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::CircleShape shape2(100.f);
+	sf::CircleShape shape3(100.f);
+	sf::CircleShape shape4(100.f);
+	sf::RectangleShape shape5(sf::Vector2f(400.0f, 100.0f));
+	shape.setFillColor(sf::Color::Red);
+	shape2.setFillColor(sf::Color::Green);
+	shape3.setFillColor(sf::Color::Yellow);
+	shape4.setFillColor(sf::Color::Red);
+	shape5.setFillColor(sf::Color::White);
+	shape2.setPosition(0.0f, 200.0f);
+	shape3.setPosition(200.0f, 0.0f);
+	shape4.setPosition(200.0f, 200.0f);
+	shape5.setPosition(0, 150.0f);
 	
 	while (window.isOpen())
 	{
@@ -18,6 +31,10 @@ int main()
 		}
 		window.clear();
 		window.draw(shape);
+		window.draw(shape2);
+		window.draw(shape3);
+		window.draw(shape4);
+		window.draw(shape5);
 		window.display();
 	}
 	return 0;
